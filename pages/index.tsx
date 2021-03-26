30b09383dbcc4e6462972faaea5cd71ce4b86b89
Shyam Krishn <shyam.dubey@crownstack.com>
2021-03-26 13:30:13 +0530

    some more fixer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
-import Head from "next/head";
 import Dashboard from "../components/Dashboard";
 import SignIn from "../components/Login";
-import styles from "../styles/Home.module.css";
 
 const isLoggedIn = true;
 
